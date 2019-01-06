d4490c9dcfb4d7a436dfa5768c090887a98ff6a9
Shante <shante.austin19@gmail.com>
2019-01-05 19:20:22 -0500

    Finished my projects section

---
+++ app.js
@@ -0,0 +1,29 @@
+
+  
+    var slideContainer = $('.slide-container');
+
+    slideContainer.slick();
+
+    $('.project-card__image img').hide();
+    $('.slick-active').find('.project-card img').fadeIn(200);
+    
+
+    // On before slide change
+    slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+      $('.slick-active').find('.project-card img').fadeOut(1000);
+    });
+  
+    
+    // On after slide change
+    slideContainer.on('afterChange', function(event, slick, currentSlide) {
+      $('.slick-active').find('.project-card img').fadeIn(200);
+    });
+
+
+
+
+  
+  
+  
+  
+  
