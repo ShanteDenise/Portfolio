1ba4ddf099a9cc3af3a98de9057a6c859e018fe0
Shante <shante.austin19@gmail.com>
2019-01-06 18:36:14 -0500

    Added animation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-    emergence.init();
+    
     var slideContainer = $('.slide-container');
 
     slideContainer.slick();
@@ -18,6 +18,31 @@
     slideContainer.on('afterChange', function(event, slick, currentSlide) {
       $('.slick-active').find('.project-card img').fadeIn(200);
     });
+
+    // Initiate Emergence
+emergence.init({
+  container: window,
+  reset: true,
+  handheld: true,
+  throttle: 250,
+  elemCushion: 0.15,
+  offsetTop: 0,
+  offsetRight: 50,
+  offsetBottom: 0,
+  offsetLeft: 0,
+  callback: function(element, state) {
+    if (state === 'noreset') {
+      $('.about-para').hide();
+      console.log('state is hidden with no reset')
+    } else if (state === 'reset') {
+      $('.about-para').show(300)
+      console.log('Element is hidden with reset.');
+    } else if (state === 'visible') {
+      $('.about-para').show(1400);
+      console.log('Element is visible.');
+    }
+  }
+});
 
 
 
