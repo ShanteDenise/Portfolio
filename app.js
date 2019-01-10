9f2cdad61e3b56c9bcea4e9443fe93f67d712fa1
Shante <shante.austin19@gmail.com>
2019-01-10 00:50:54 -0500

    Added search bar for skills

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
     
     var slideContainer = $('.slide-container');
 
-    slideContainer.slick();
+    slideContainer.slick({
+      autoplay:true,
+      autoplaySpeed: 4000,
+    });
+
 
     $('.project-card__image img').hide();
     $('.slick-active').find('.project-card img').fadeIn(200);
@@ -44,10 +48,63 @@ emergence.init({
   }
 });
 
+//Search skills bar 
+var mySkills = ["HTML", "CSS", "Javascript", "JQuery", "Sass", "Node", "Express", "Python", "Django", "React", "Handlebars", "Redux"];
+var notSkills = ["C#", ".Net", "Angular"];
+var trueSkills = $("skillSearch_True");
+var falseSkills = $("skillSearch_False");
+
+
+const yourArray =[];
+const skillMatch = [];
+const notSkillMatch =[]
+
+var searchField = $('#searchTerm');
+var searchButton = $('#searchButton');
+
+searchButton.on("click",function skillsearch() {
+  var searchFieldInput = searchField.val();
+  console.log("Search Field Input:", searchFieldInput)
+  eachInputValue = searchFieldInput.split(',');
+  console.log("Each value:", eachInputValue);
+
+
+for(let x = 0; x < eachInputValue.length; x++){
+  upperWord = eachInputValue[x].toUpperCase();
+
+  for(let i = 0; i < mySkills.length; i++){
+    upperWord === mySkills[i].toUpperCase() ? skillMatch.push(mySkills[i].toUpperCase()) : "";
+    console.log(upperWord)
+  }
+  for (y = 0; y < notSkills.length; y++){
+    upperWord === notSkills[y].toUpperCase() ? notSkillMatch.push(notSkill[y].toUpperCase()) : "";
+    console.log("Yes it matches",skillMatch)
+  }
+  }
+
+
+if(skillMatch.length !== 0) {
+  alert(`Yes I can help with: ${skillMatch}`).map()
+} else {
+  console.log('none')
+  // alert("I cant help with that")
+}
+if (notSkillMatch.length !== 0) 
+  falseSkills.html("Sorry, I cannot help with: ${notSkillMatch}").map(skill => `<li>${skill}</li>`)
+ else {
+  falseSkills.hide()
+}
+
+})
+ 
+ 
+// };
 
 
 
-  
+
+
+
   
   
   
